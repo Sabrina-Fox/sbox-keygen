fde875e1503eed46188c93d5552330b68a859109
SabrinaRDC <SabrinaRDC.Dev+github@gmail.com>
2024-05-06 01:23:10 +0800

    updated version

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-let version = "v3.4.2"
+let version = "v4.0.2"
 let keyOut = document.getElementById('key_display');
 let button = document.getElementById('generate_button');
 let name = document.getElementById('name');
